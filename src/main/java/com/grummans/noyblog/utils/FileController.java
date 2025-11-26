742451df443381980d3f08279491379fb421f903
grummans <nguyenhaikiller@gmail.com>
2025-11-26 23:00:02 +0700

    implement file upload functionality in FileController; update pom.xml to include okhttp dependency

--- src/main/java/com/grummans/noyblog/utils/FileController.java
+++ src/main/java/com/grummans/noyblog/utils/FileController.java
@@ -2,14 +2,19 @@ package com.grummans.noyblog.utils;
 
 import com.grummans.noyblog.configuration.ApiResponse;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 @RestController("/file")
@@ -21,12 +26,21 @@ public class FileController {
 
     private final MinioClient minioClient;
 
-    @PatchMapping("/upload")
-    public ApiResponse<String> uploadFile(@RequestParam MultipartFile file) throws IOException {
+    @PostMapping("/upload")
+    public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        ApiResponse<String> response = new ApiResponse<>();
 
-        String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        var inputStream = file.getInputStream();
 
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-        return null;
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .stream(inputStream, file.getSize(), -1)
+                        .contentType(file.getContentType())
+                        .build()
+        );
     }
 }
