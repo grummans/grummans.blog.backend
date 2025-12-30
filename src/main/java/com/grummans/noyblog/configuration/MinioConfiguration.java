ded35ebe1616890551c423f34ba8685ef238f542
nnhai <nnhai@dainls.com>
2025-12-30 15:56:23 +0700

    chore: Update application configuration files to use environment variables and add MinIO settings

--- src/main/java/com/grummans/noyblog/configuration/MinioConfiguration.java
+++ src/main/java/com/grummans/noyblog/configuration/MinioConfiguration.java
@@ -4,10 +4,8 @@ import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:minio/MinioConfig.properties")
 public class MinioConfiguration {
 
     @Value("${minio.accessKey}")
